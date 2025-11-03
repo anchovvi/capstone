a46ad811d788c1ff0dbac0d1d9838b562601d6b0
minju-sin <sinminju04@gmail.com>
2025-11-03 20:49:11 +0900

    Update ServiceRight.js

--- frontend/src/components/ui/ServiceRight.js
+++ frontend/src/components/ui/ServiceRight.js
@@ -31,6 +31,7 @@ function ServiceRight() {
     };
 
 
+    
     // 테이블 행 클릭 시 팝업을 표시하고 선택한 영수증 데이터를 설정하는 함수
     const handleRowClick = async (idorder) => {
         console.log("클릭 이벤트 발생:", idorder);
